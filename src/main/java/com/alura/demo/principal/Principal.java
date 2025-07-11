9cf77b20024c7dde2e206ed24e6c2b78bfa12cdc
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-07-11 01:14:01 -0300

    Top 10 libros mas descargados

--- src/main/java/com/alura/demo/principal/Principal.java
+++ src/main/java/com/alura/demo/principal/Principal.java
@@ -1,8 +1,11 @@
 package com.alura.demo.principal;
 
 import com.alura.demo.model.Datos;
+import com.alura.demo.model.DatosLibros;
 import com.alura.demo.service.ConsumoAPI;
 import com.alura.demo.service.ConvierteDatos;
+
+import java.util.Comparator;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/" ;
@@ -15,5 +18,14 @@ public class Principal {
         System.out.println(json);
         var datos = conversor.obtenerDatos(json, Datos.class);
         System.out.println(datos);
+
+        //Top 10 libros más descargados
+        System.out.println("Top 10 libros más descargados: ");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l->l.titulo().toUpperCase())
+                .forEach(System.out::println);
+
     }
 }
